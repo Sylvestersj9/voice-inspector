519ccf3298af8665a498571a630607139699f756
Sylvestersj9 <janvesylvester@gmail.com>
2025-12-16 16:51:49 +0000

    fix(runtime): import useEffect in Index

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,4 +1,4 @@
-﻿import { useState, useMemo } from "react";
+﻿import { useState, useMemo, useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/hooks/use-toast";
